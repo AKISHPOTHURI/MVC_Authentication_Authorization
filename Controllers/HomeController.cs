9aea554c04b83f5eb0cb30be4f8d53c35b4bc425
akish.pothuri <akish.pothuri@innovasolutions.com>
2023-03-07 16:39:47 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Basics.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+        public IActionResult Authenticate()
+        {
+            var pocClaims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Akish"),
+                new Claim(ClaimTypes.Email, "akish@acs.com"),
+            };
+
+            var pocIdentity = new ClaimsIdentity(pocClaims, "POC Identity");
+
+            var userPrincipal = new ClaimsPrincipal(new[] { pocIdentity });
+
+            HttpContext.SignInAsync(userPrincipal);
+            return RedirectToAction("Index");
+        }
+    }
+}
